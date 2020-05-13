033359ee726f48dd04d4f09552d9ddb7c6af2879
Timothy Coish <tdcoish@gmail.com>
2020-05-12 17:09:48 -0700

    Mouse Follow
    Mouse cursor follows position, also pieces made.

---
+++ OGK_Chess/Assets/Scripts/UI/UI_Mouse.cs
@@ -0,0 +1,18 @@
+﻿/*************************************************************************************
+
+*************************************************************************************/
+using UnityEngine;
+
+public class UI_Mouse : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(cursorPos.x, cursorPos.y);
+    }
+}
